1d805b93be490dd2582745db966f9a5c535f62ad
christar <dev.christar@gmail.com>
2021-02-07 11:47:24 +0100

    [proj1g] assert only fails when removing item is not correct or size is not correct

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -8,32 +8,34 @@ public class TestArrayDequeGold {
         StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
         String errorSequence = "";
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1000; i++) {
             double rng = StdRandom.uniform();
 
             if (rng < 0.25) {
                 errorSequence += "addFirst(" + i + ")\n";
                 ads.addFirst(i);
                 sad.addFirst(i);
-                assertEquals(errorSequence, ads.get(0), sad.get(0));
+                assertEquals(errorSequence, ads.size(), sad.size());
             } else if (rng < 0.5) {
                 errorSequence += "addLast(" + i + ")\n";
                 ads.addLast(i);
                 sad.addLast(i);
-                assertEquals(errorSequence, ads.get(ads.size() - 1), sad.get(sad.size() - 1));
+                assertEquals(errorSequence, ads.size(), sad.size());
             } else if (rng < 0.75) {
                 if (ads.size() != 0 && sad.size() != 0) {
                     errorSequence += "removeFirst()\n";
-                    Integer ads_item = ads.removeFirst();
-                    Integer sad_item = sad.removeFirst();
-                    assertEquals(errorSequence, ads_item, sad_item);
+                    Integer adsItem = ads.removeFirst();
+                    Integer sadItem = sad.removeFirst();
+                    assertEquals(errorSequence, ads.size(), sad.size());
+                    assertEquals(errorSequence, adsItem, sadItem);
                 }
             } else {
                 if (ads.size() != 0 && sad.size() != 0) {
                     errorSequence += "removeLast()\n";
-                    Integer ads_item = ads.removeLast();
-                    Integer sad_item = sad.removeLast();
-                    assertEquals(errorSequence, ads_item, sad_item);
+                    Integer adsItem = ads.removeLast();
+                    Integer sadItem = sad.removeLast();
+                    assertEquals(errorSequence, ads.size(), sad.size());
+                    assertEquals(errorSequence, adsItem, sadItem);
                 }
             }
         }
